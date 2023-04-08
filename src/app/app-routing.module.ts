46fccae038596d04bb2b9b051b71431c3e1a30e4
Ryan Kellmer <ryankellmer@gmail.com>
2023-04-08 14:19:20 -0700

    Created App and Added Login, Inventory, and Taxes Pages.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginUserComponent } from './pages/login-user/login-user.component';
+import { InventoryComponent } from './pages/inventory/inventory.component';
+import { TaxesComponent } from './pages/taxes/taxes.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'login', component: LoginUserComponent },
+  { path: 'inventory', component: InventoryComponent },
+  { path: 'taxes', component: TaxesComponent },
+{ path: '', redirectTo: 'login', pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { TitleComponent } from './components/title/title.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InventoryComponent } from './pages/inventory/inventory.component';
+import { TaxesComponent } from './pages/taxes/taxes.component';
+import { LoginUserComponent } from './pages/login-user/login-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    TitleComponent,
+    InventoryComponent,
+    TaxesComponent,
+    LoginUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatCardModule,
+    MatIconModule,
+    MatExpansionModule,
+    MatListModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatBadgeModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
